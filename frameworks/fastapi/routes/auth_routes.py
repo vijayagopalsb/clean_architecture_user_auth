92492c53dd43f5fe6a2b6ab7b1eb74f1ffbdcc0e
vijayagopalsb <vijayagopal.sb@gmail.com>
2025-05-20 21:47:17 +0530

    REST API  post updated

--- frameworks/fastapi/routes/auth_routes.py
+++ frameworks/fastapi/routes/auth_routes.py
@@ -1,22 +1,27 @@
 # File: frameworks/fastapi/routes/auth_routes.py
+
 from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
+from frameworks.fastapi.dto.user_dto import UserModel
+from use_cases.register_user import RegisterUserUseCase
 from adapters.repositories.user_repository_impl import UserRepositoryImpl
 from adapters.hashing.bcrypt_adapter import BcryptHasher
-from use_cases.register_user import RegisterUserUseCase
+from domain.entities.user import User
+
 
 router = APIRouter()
 
-class RegisterRequest(BaseModel):
-    username: str
-    password: str
-    email: str
-
 @router.post("/register")
-def register_user(request: RegisterRequest):
-    
+def register_user(user_input: UserModel):
+
     use_case = RegisterUserUseCase(UserRepositoryImpl(), BcryptHasher())
+    # Convert DTO to domain User
+    user_entity = User(
+        username=user_input.username,
+        password=user_input.password,
+        email=user_input.email
+    )
+
     try:
-        return use_case.execute(request.username, request.password, request.email)
+        return use_case.execute(user_entity)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
